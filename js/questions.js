5373e5bc2bcb7d3a21e14ae81a5fb5eaf8f0ee32
urbinapro <urbinapro@gmail.com>
2017-01-30 17:02:36 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -35,7 +35,7 @@ function gestionarXml(dadesXml){
  
  document.getElementById("selecttitle").innerHTML = xmlDoc.getElementsByTagName("title")[1].childNodes[0].nodeValue;
  
- var select = xmlDoc.getElementsByTagName("select")[0];
+ var select = xmlDoc.getElementsByTagName("select")[0].childNodes[0];
  
  var option =  xmlDoc.getElementsByTagName("option")[0].childNodes[0];
  option.text = xmlDoc.getElementsByTagName("option")[0].childNodes[0].nodeValue;
